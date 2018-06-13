9472f728bb2ed2822106e3ba0fc131599c55abf2
Rob Myers <rob@robmyers.org>
2018-06-13 16:56:36 -0700

    Revert to older page layout for cover.

--- themes/bombadil/page-cover.php
+++ themes/bombadil/page-cover.php
@@ -1,24 +1,20 @@
-<?php /* Template Name: Cover */
+<?php
+	get_header();
+	$metadata = pb_get_book_information();
+	if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))):
+	if (have_posts()) the_post();
+?>
 
-// From pressbooks-book
 
-$book_structure = pb_get_book_structure();
-$book_information = pb_get_book_information();
+	<?php get_template_part('page-cover', 'top-block'); ?>
+	<?php get_template_part('page-cover', 'second-block'); ?>
+	<?php get_template_part('page-cover', 'third-block'); ?>
 
-get_header();
+<?php else: ?>
 
-if ( pb_is_public() ) {
-        include( locate_template( 'partials/content-cover-book-header.php' ) );
-        include( locate_template( 'partials/content-cover-toc.php' ) );
-        include( locate_template( 'partials/content-cover-book-info.php' ) );
-        include( locate_template( 'partials/content-cover-metadata.php' ) );
-} else { ?>
-        <section class="block private-block">
-                <?php pb_private(); ?>
-        </section>
-<?php }
-/**     Insert content before cover footer.
- * @since 2.0.0
- */
-do_action( 'pb_book_cover_before_footer' );
-get_footer();
+	<?php get_template_part('page-cover', 'private-block'); ?>
+
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
