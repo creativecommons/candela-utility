019e82848d3c2c926775cfdd46b701b0b129a450
Rob Myers <rob@robmyers.org>
2018-06-13 17:12:48 -0700

    Revert and update cover page code.

--- themes/bombadil/page-cover.php
+++ themes/bombadil/page-cover.php
@@ -1,20 +1,27 @@
 <?php
-	get_header();
-	$metadata = pb_get_book_information();
-	if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))):
-	if (have_posts()) the_post();
-?>
+// From pressbooks-book
+$book_structure = pb_get_book_structure();
+$book_information = pb_get_book_information();
 
+get_header();
+$metadata = pb_get_book_information();
+if (get_option('blog_public') == '1'
+    || (get_option('blog_public') == '0'
+        && current_user_can_for_blog($blog_id, 'read'))) {
 
-	<?php get_template_part('page-cover', 'top-block'); ?>
-	<?php get_template_part('page-cover', 'second-block'); ?>
-	<?php get_template_part('page-cover', 'third-block'); ?>
+    if (have_posts()) { the_post(); }
 
-<?php else: ?>
+    include( locate_template( 'partials/content-cover-book-header.php' ) );
 
-	<?php get_template_part('page-cover', 'private-block'); ?>
+    /*get_template_part('page-cover', 'top-block');*/
+    /* get_template_part('page-cover', 'second-block'); */
+    get_template_part('page-cover', 'third-block');
 
+} else {
 
-<?php endif; ?>
+    get_template_part('page-cover', 'private-block');
 
-<?php get_footer(); ?>
+}
+
+/*do_action( 'pb_book_cover_before_footer' );
+  get_footer();*/
